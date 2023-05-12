c1225ed391ed08ac24b3879c9953239b2149e44e
Mr-Meow-ZA <108538294+Mr-Meow-ZA@users.noreply.github.com>
2023-05-12 14:10:48 +0200

    added groups

--- main.ts
+++ main.ts
@@ -1,39 +1,35 @@
+/******************************************************************
+ * Extension for Chop Chop microbit App. 
+ * App by Nick Bradley and Extension by Rapha Pretorius
+ * Author: Rapha Pretorius
+ * Email: raphapretorius@gmail.com
+******************************************************************/
+
 enum ButtonOption {
-    Up,
-    Right,
-    Down,
-    Left
+    Up,Right,Down,Left
 }
 enum CompassButtonOption {
     N,NE,E,SE,S,SW,W,NW
 }
 enum LightOption {
-    Light1,
-    Light2,
-    Light3
+    Light1,Light2,Light3
 }
 
 enum LightState {
-    On,
-    Off
+    On,Off
 }
-
 enum ToggleOption {
-    Toggle1,
-    Toggle2,
-    Toggle3
+    Toggle1,Toggle2,Toggle3
 }
 
 
+//% weight=40 color=#226025 icon="\uf10b" block="microbit App"
+//% groups=['4 Button Navigation', 'Compass Navigation', 'Slider and Rx Values', 'Send and Display on App', 'Set Light Icon', 'Toggle State', 'Custom Receive then Do block']
+namespace microbitApp {
 
-
-
-//% weight=40 color=#226025 icon="\uf10b" block="microbitAPP"
-namespace microbitAPP {
-
+    // define global variables
     let optionStrings: string[] = ["BF", "BR", "BB", "BL"];
     let compassOptionStrings: string[] = ["BF", "BFR", "BR", "BBR", "BB", "BBL", "BL", "BFL"]
-
     let stateToggleOne = 0;
     let stateToggleTwo = 0;
     let stateToggleThree = 0;
@@ -48,7 +44,6 @@ namespace microbitAPP {
     let rx2 = "";
 
     bluetooth.startUartService();
- 
 
     bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Comma), function () {
         let data = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Comma));
@@ -78,93 +73,97 @@ namespace microbitAPP {
                 stateToggleThree = parseInt(rx2);
             }
 
-            // Write values to the serial monitor
+            // Write values to the serial monitor for testing purpose
             serial.writeLine("rx1: " + rx1);
             serial.writeLine("rx2: " + rx2);
         }
     });
-
+    
     /**
      * Get the value of rx1 and rx2 variable.
      */
+    //% group="Slider and Rx Values"
     //% block="get rx1"
     export function getRx1(): string {
         return rx1;
     }
+    //% group="Slider and Rx Values"
     //% block="get rx2"
     export function getRx2(): string {
         return rx2;
     }
+    //% group="Slider and Rx Values"
     //% block="slider X value"
     export function getSliderX(): number {
         return sliderValX;
     }
+    //% group="Slider and Rx Values"
     //% block="slider Y value"
     export function getSliderY(): number {
         return sliderValY;
     }
+    //% group="Slider and Rx Values"
     //% block="slider Z value"
     export function getSliderZ(): number {
         return sliderValZ;
     }
 
+    //% group="Send and Display on App"
     //% block="Display number %value on App"
     export function sendValueOnceToApp(value: number): void {
         bluetooth.uartWriteString("V1#" + convertToText(value));
     }
+    //% group="Send and Display on App"
     //% block="Display string $value on App"
     export function sendStringToApp(value: string): void {
         bluetooth.uartWriteString("V1#" + value);
     }
 
+    
     /**
      * Custom block to handle different button options based on rx1 values.
      * @param option - The button option to match.
      * @param handler - The code to run when the option is matched.
      */
+    //% group="4 Button Navigation"
     //% block="When button %option is pressed down"
     export function onButtonOption(option: ButtonOption, handler: () => void): void {
         basic.forever(function () {
-            if (rx2 == "D"){
-            if (rx1 == optionStrings[option]) {
+            if (rx1 == optionStrings[option] && rx2 == "D") {
                 handler();
-            }
             }
         });
     }
+    //% group="4 Button Navigation"
     //% block="When button %option is released"
     export function onButtonUpOption(option: ButtonOption, handler: () => void): void {
         basic.forever(function () {
-            if (rx2 == "U") {
-                if (rx1 == optionStrings[option]) {
-                    handler();
-                }
+            if (rx1 == optionStrings[option] && rx2 == "U") {
+                handler();
             }
         });
     }
 
+    //% group="Compass Navigation"
     //% block="When compass button %option is pressed down"
     export function compassButtonDown(option: CompassButtonOption, handler: () => void): void {
         basic.forever(function () {
-            if (rx2 == "D") {
-                if (rx1 == compassOptionStrings[option]) {
-                    handler();
-                }
+            if (rx1 == compassOptionStrings[option] && rx2 == "D") {
+                handler();
             }
         });
     }
+    //% group="Compass Navigation"
     //% block="When compass button %option is released"
     export function compassButtonUp(option: CompassButtonOption, handler: () => void): void {
         basic.forever(function () {
-            if (rx2 == "U") {
-                if (rx1 == compassOptionStrings[option]) {
-                    handler();
-                }
+            if (rx1 == compassOptionStrings[option] && rx2 == "U") {
+                handler();
             }
         });
     }
 
-
+    //% group="Custom Receive then Do block"
     //% block="Do when rx1 is $rxOne and rx2 is $rxTwo"
     export function onCustomRx1Rx2(rxOne:string, rxTwo:string, handler:() => void): void {
         basic.forever(function() {
@@ -173,12 +172,12 @@ namespace microbitAPP {
             }
         });
     }
-    /**
-    * Send a string command to turn on or off the specified light.
-    * Update the state variable accordingly.
+
+    /*
     * @param light - The light to control.
     * @param state - The state of the light (On or Off).
     */
+    //% group="Set Light Icon"
     //% block="Set $light $state"
     export function setLightState(light: LightOption, state: LightState): void {
         let command = "";
@@ -198,7 +197,6 @@ namespace microbitAPP {
                 stateVariable = stateLightThree;
                 break;
         }
-
         if (state === LightState.On) {
             command += "#1";
             stateVariable = 1;
@@ -206,17 +204,16 @@ namespace microbitAPP {
             command += "#0";
             stateVariable = 0;
         }
-
         bluetooth.uartWriteString(command);
     }
 
-
-    /**
+    /*
      * Get the state of the specified toggle.
      * Returns the value of the toggle (0 for off, 1 for on).
      * @param toggle - The toggle to check.
      * @returns The current state of the toggle (0 for off, 1 for on).
      */
+    //% group="Toggle State"
     //% block="Get state of $toggle"
     export function getToggleState(toggle: ToggleOption): number {
         let state = 0;
@@ -232,7 +229,6 @@ namespace microbitAPP {
                 state = stateToggleThree;
                 break;
         }
-
         return state;
     }
 
