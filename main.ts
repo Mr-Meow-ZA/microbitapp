3506a16e5435ff7f54d4a9f8bdf60527acfc0eb4
Mr-Meow-ZA <108538294+Mr-Meow-ZA@users.noreply.github.com>
2023-05-12 09:23:47 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
-basic.forever(function () {
-	
-})
+/*
+* Test Extension for the Chop Chop Microbit APP
+* Goal is to package everything into an easy to use block extension.
+*/
+
+//% weight=40 color=#226025 icon="\uf110" block="microbitAPP"
+
+namespace microbitAPP {
+    // set global variables
+    let stateToggleOne : number
+    let stateToggleTwo : number
+    let stateToggleThree : number
+    let stateLightOne : number
+    let stateLightTwo : number
+    let stateLightThree : number
+    let stateButtonDown : number
+    let sliderValX : number
+    let sliderValY : number
+    let sliderValZ : number
+    
+
+}
